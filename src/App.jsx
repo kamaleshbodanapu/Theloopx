038b1f26c75802ad8bb584b7a45e641dcf7a463d
kamaleshrj <kamaleshbodanapu7075@gmail.com>
2024-03-23 16:10:46 +0530

    theloopx maintaince application

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+import './App.css'
+
+function App() {
+  return (
+    <div className='PageModule'>
+      <center>
+        <br/><br/><br/><br/>
+        <h1>Site Under Maintance</h1>
+        <p>We had any quries contact below details</p>
+        <h2>mail: Info@theloopx.com </h2> 
+      </center>
+      
+      
+    </div>
+  )
+}
+
+export default App;
